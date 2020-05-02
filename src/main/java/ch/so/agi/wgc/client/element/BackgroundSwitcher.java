4859e892859cdf3ff7d411d4d446837d3a390a88
Stefan Ziegler <edi.gonzales@gmail.com>
2020-05-02 22:40:07 +0200

    start with BackgroundSwitcher element

---
+++ src/main/java/ch/so/agi/wgc/client/element/BackgroundSwitcher.java
@@ -0,0 +1,23 @@
+package ch.so.agi.wgc.client.element;
+
+import org.jboss.elemento.Attachable;
+import org.jboss.elemento.IsElement;
+
+import elemental2.dom.HTMLElement;
+import elemental2.dom.MutationRecord;
+
+public class BackgroundSwitcher implements IsElement<HTMLElement>, Attachable {
+
+    @Override
+    public void attach(MutationRecord mutationRecord) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public HTMLElement element() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
